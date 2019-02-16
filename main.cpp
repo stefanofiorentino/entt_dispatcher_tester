f8851365651527923b62534287f7a42c5fa16dd6
Stefano Fiorentino <stefano.fiore84@gmail.com>
2019-02-16 12:15:35 +0100

    Initial commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include "entt/src/entt/entt.hpp"
 
 int main()
 {
+    // define a general purpose dispatcher that works with naked pointers
+    entt::dispatcher dispatcher{};
+
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
