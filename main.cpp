cabc9351f44724b590a464f998007055b40f482c
Stefano Fiorentino <stefano.fiore84@gmail.com>
2019-02-16 12:23:59 +0100

    introducing entt::dispatcher
    Signed-off-by: Stefano Fiorentino <stefano.fiore84@gmail.com>

--- main.cpp
+++ main.cpp
@@ -6,6 +6,41 @@ int main()
     // define a general purpose dispatcher that works with naked pointers
     entt::dispatcher dispatcher{};
 
-    std::cout << "Hello, World!" << std::endl;
+    struct an_event
+    {
+        int value;
+
+        an_event() :
+                value(-1)
+        {};
+
+        an_event(int value) :
+                value(value)
+        {};
+
+    };
+    struct another_event
+    {
+    };
+
+    struct listener
+    {
+        void receive(const an_event &e)
+        { std::cout << "Hello " << e.value << std::endl; }
+
+        void method(const another_event &a)
+        {std::cout << "Hello, World!" << std::endl; }
+    };
+
+    listener listener;
+    dispatcher.sink<an_event>().connect<&listener::receive>(&listener);
+    dispatcher.sink<another_event>().connect<&listener::method>(&listener);
+
+    dispatcher.trigger<an_event>(42);
+    dispatcher.trigger<another_event>();
+
+    dispatcher.sink<an_event>().disconnect<&listener::receive>(&listener);
+    dispatcher.sink<another_event>().disconnect<&listener::method>(&listener);
+
     return 0;
 }
