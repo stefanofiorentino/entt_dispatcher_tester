ecac934e9d37f069b64de1760ec649078f7bb486
Stefano Fiorentino <stefano.fiore84@gmail.com>
2019-09-09 17:11:47 +0200

    clean main example
    Signed-off-by: Stefano Fiorentino <stefano.fiore84@gmail.com>

--- main.cpp
+++ main.cpp
@@ -2,45 +2,27 @@
 #include "entt/src/entt/entt.hpp"
 
 struct an_event
-{
-    int value;
-
-    an_event() :
-            value(-1)
-    {};
-
-    an_event(int value) :
-            value(value)
-    {};
-
-};
-struct another_event
-{
-};
+{};
 
 struct listener
 {
-    void receive(const an_event &e)
-    { std::cout << "Hello " << e.value << " an_event" << std::endl; }
-
-    void method(const another_event &a)
-    { std::cout << "Hello, another_event!" << std::endl; }
+    void receive(const an_event &e, int value)
+    { std::cout << "Hello " << value << " an_event" << std::endl; }
 };
 
 int main()
 {
     // define a general purpose dispatcher that works with naked pointers
-    entt::dispatcher dispatcher{};
+    entt::dispatcher<int> dispatcher{};
 
     listener listener;
     dispatcher.sink<an_event>().connect<&listener::receive>(&listener);
-    dispatcher.sink<another_event>().connect<&listener::method>(&listener);
-
     dispatcher.trigger<an_event>(42);
-    dispatcher.trigger<another_event>();
-
+    dispatcher.enqueue<an_event>();
+    dispatcher.enqueue<an_event>();
+    dispatcher.enqueue<an_event>();
+    dispatcher.update<an_event>(1);
     dispatcher.sink<an_event>().disconnect<&listener::receive>(&listener);
-    dispatcher.sink<another_event>().disconnect<&listener::method>(&listener);
 
     return 0;
 }
